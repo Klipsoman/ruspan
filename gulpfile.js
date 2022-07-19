e70f65a4bb1c38a86f57cbb602dbae038e143f0d
Andrew <Andrew1@work.kodeks.ru>
2022-07-19 10:10:07 +0300

    some fixes

--- gulpfile.js
+++ gulpfile.js
@@ -7,23 +7,23 @@ const { src, dest, parallel, series, watch } = require("gulp");
 const browserSync = require("browser-sync").create();
 
 // Подключаем gulp-concat
-const concat = require("gulp-concat");
+// const concat = require("gulp-concat");
 
 // Подключаем gulp-uglify-es
-const uglify = require("gulp-uglify-es").default;
+// const uglify = require("gulp-uglify-es").default;
 
 // Подключаем модули gulp-sass и gulp-less
 const sass = require("gulp-sass")(require("sass"));
-const less = require("gulp-less");
+// const less = require("gulp-less");
 
 // Подключаем Autoprefixer
-const autoprefixer = require("gulp-autoprefixer");
+// const autoprefixer = require("gulp-autoprefixer");
 
 // Подключаем модуль gulp-clean-css
 const cleancss = require("gulp-clean-css");
 
 // Подключаем compress-images для работы с изображениями
-const imagecomp = require("compress-images");
+// const imagecomp = require("compress-images");
 
 // Подключаем модуль del
 const del = require("del");
@@ -59,7 +59,7 @@ function startwatch() {
   // Мониторим файлы HTML на изменения
   watch("app/**/*.html").on("change", browserSync.reload);
   // Мониторим папку-источник изображений и выполняем images(), если есть изменения
-	watch('app/assets/images/**/*', images);
+  // watch('app/assets/images/**/*', images);
 }
 
 function styles() {
@@ -67,12 +67,12 @@ function styles() {
     src("app/" + preprocessor + "/style." + preprocessor + "") // Выбираем источник: "app/sass/main.sass" или "app/less/main.less"
       .pipe(eval("sass")()) // Преобразуем значение переменной "preprocessor" в функцию
       // .pipe(concat('app.min.css')) // Конкатенируем в файл app.min.js
-      .pipe(
-        autoprefixer({ overrideBrowserslist: ["last 5 versions"], grid: true })
-      ) // Создадим префиксы с помощью Autoprefixer
+      // .pipe(
+      //   autoprefixer({ overrideBrowserslist: ["last 5 versions"], grid: true })
+      // ) // Создадим префиксы с помощью Autoprefixer
       .pipe(
         cleancss({
-          level: { 1: { specialComments: 0 } } /* , format: 'beautify' */,
+          level: { 1: { specialComments: 0 } }, format: 'beautify',
         })
       ) // Минифицируем стили
       .pipe(dest("app/")) // Выгрузим результат в папку "app/css/"
@@ -98,48 +98,48 @@ function cleandist() {
   return del("dist/**/*", { force: true }); // Удаляем все содержимое папки "dist/"
 }
 
-async function images() {
-  imagecomp(
-    "app/assets/images/**/*", // Берём все изображения из папки источника
-    "app/dest/images/", // Выгружаем оптимизированные изображения в папку назначения
-    { compress_force: false, statistic: true, autoupdate: true },
-    false, // Настраиваем основные параметры
-    { jpg: { engine: "mozjpeg", command: ["-quality", "75"] } }, // Сжимаем и оптимизируем изображеня
-    { png: { engine: "pngquant", command: ["--quality=75-100", "-o"] } },
-    { svg: { engine: "svgo", command: "--multipass" } },
-    {
-      gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] },
-    },
-    function (err, completed) {
-      // Обновляем страницу по завершению
-      if (completed === true) {
-        browserSync.reload();
-      }
-    }
-  );
-}
+// async function images() {
+//   imagecomp(
+//     "app/assets/images/**/*", // Берём все изображения из папки источника
+//     "app/dest/images/", // Выгружаем оптимизированные изображения в папку назначения
+//     { compress_force: false, statistic: true, autoupdate: true },
+//     false, // Настраиваем основные параметры
+//     { jpg: { engine: "mozjpeg", command: ["-quality", "75"] } }, // Сжимаем и оптимизируем изображеня
+//     { png: { engine: "pngquant", command: ["--quality=75-100", "-o"] } },
+//     { svg: { engine: "svgo", command: "--multipass" } },
+//     {
+//       gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] },
+//     },
+//     function (err, completed) {
+//       // Обновляем страницу по завершению
+//       if (completed === true) {
+//         browserSync.reload();
+//       }
+//     }
+//   );
+// }
 
-function cleanimg() {
-	return del('dist/assets/images/**/*', { force: true }) // Удаляем все содержимое папки "app/images/dest/"
-}
+// function cleanimg() {
+//   return del('dist/assets/images/**/*', { force: true }) // Удаляем все содержимое папки "app/images/dest/"
+// }
 
-	// Экспортируем функцию browsersync() как таск browsersync. Значение после знака = это имеющаяся функция.
-  exports.browsersync = browsersync;
- 
-  // Экспортируем функцию scripts() в таск scripts
-  exports.scripts = scripts;
-   
-  // Экспортируем функцию styles() в таск styles
-  exports.styles = styles;
-   
-  // Экспорт функции images() в таск images
-  exports.images = images;
-   
-  // Экспортируем функцию cleanimg() как таск cleanimg
-  exports.cleanimg = cleanimg;
-   
-  // Создаем новый таск "build", который последовательно выполняет нужные операции
-  exports.build = series(cleandist, styles, scripts, images, buildcopy);
-   
-  // Экспортируем дефолтный таск с нужным набором функций
-  exports.default = parallel(styles, scripts, browsersync, startwatch);
+// Экспортируем функцию browsersync() как таск browsersync. Значение после знака = это имеющаяся функция.
+exports.browsersync = browsersync;
+
+// Экспортируем функцию scripts() в таск scripts
+exports.scripts = scripts;
+
+// Экспортируем функцию styles() в таск styles
+exports.styles = styles;
+
+// Экспорт функции images() в таск images
+// exports.images = images;
+
+// Экспортируем функцию cleanimg() как таск cleanimg
+// exports.cleanimg = cleanimg;
+
+// Создаем новый таск "build", который последовательно выполняет нужные операции
+exports.build = series(cleandist, styles, scripts, buildcopy);
+
+// Экспортируем дефолтный таск с нужным набором функций
+exports.default = parallel(styles, scripts, browsersync, startwatch);
